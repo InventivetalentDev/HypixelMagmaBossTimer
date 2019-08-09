409f74fb1d0d26939b82be201badbd960ba37e87
inventivetalent <mail@inventivetalent.org>
2019-08-09 13:27:58 +0200

    fix

--- add_spawn.php
+++ add_spawn.php
@@ -6,9 +6,11 @@ if ($_SERVER['REQUEST_METHOD'] !== "POST") {
 
 include_once "db_stuff.php";
 
+$date = date("Y-m-d H:i:s");
 $rel=-1;
 $stmt = $conn->prepare("INSERT INTO hypixel_skyblock_magma_timer_spawns (rel,time) VALUES(?,?)");
 $stmt->bind_param("is", $rel, $date);
 $stmt->execute();
 $stmt->close();
 
+echo "added";
