d566ec94666e1ada68f1f926881374e9c833c60d
inventivetalent <mail@inventivetalent.org>
2019-08-10 19:30:51 +0200

    link

--- index.php
+++ index.php
@@ -100,6 +100,8 @@
 
         </div>
 
+      <span style="position: fixed; bottom: 4px; left: 4px; color: gray;">Created by <a target="_blank" href="https://inventivetalent.org/?utm_source=hypixel_magma_tracker">inventivetalent</a></span>
+
 
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
