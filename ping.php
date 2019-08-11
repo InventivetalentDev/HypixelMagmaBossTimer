017d3029d848492b5955d0cc12731ab3ef7c1d64
inventivetalent <mail@inventivetalent.org>
2019-08-12 00:47:04 +0200

    fix

--- ping.php
+++ ping.php
@@ -14,7 +14,7 @@ $date = date("Y-m-d H:i:s");
 $activeDate = isset($lastFocused) ? date("Y-m-d H:i:s", $lastFocused) : $date;
 $rel = -1;
 $stmt = $conn->prepare("INSERT INTO hypixel_skyblock_magma_timer_pings (ip,time,active_time) VALUES(?,?,?) ON DUPLICATE KEY UPDATE time=?, active_time=?");
-$stmt->bind_param("sss", $ip, $date, $activeDate, $date, $activeDate);
+$stmt->bind_param("sssss", $ip, $date, $activeDate, $date, $activeDate);
 $stmt->execute();
 $stmt->close();
 unset($stmt);
