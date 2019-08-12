ba5dc6f943aafd18f4776cb6bc139fa9e34435cc
inventivetalent <mail@inventivetalent.org>
2019-08-12 21:08:09 +0200

    keep track of the MC username if provided

--- ping.php
+++ ping.php
@@ -6,6 +6,7 @@ if ($_SERVER['REQUEST_METHOD'] !== "POST") {
 
 $ip = $_SERVER["HTTP_CF_CONNECTING_IP"];
 $lastFocused = $_POST["lastFocused"];
+$username = isset($_POST["minecraftUser"]) ? $_POST["minecraftUser"] : "";
 
 include_once "db_stuff.php";
 
@@ -13,8 +14,8 @@ include_once "db_stuff.php";
 $date = date("Y-m-d H:i:s");
 $activeDate = isset($lastFocused) ? date("Y-m-d H:i:s", $lastFocused) : $date;
 $rel = -1;
-$stmt = $conn->prepare("INSERT INTO hypixel_skyblock_magma_timer_pings (ip,time,active_time) VALUES(?,?,?) ON DUPLICATE KEY UPDATE time=?, active_time=?");
-$stmt->bind_param("sssss", $ip, $date, $activeDate, $date, $activeDate);
+$stmt = $conn->prepare("INSERT INTO hypixel_skyblock_magma_timer_pings (ip,time,active_time,minecraftName) VALUES(?,?,?,?) ON DUPLICATE KEY UPDATE time=?, active_time=?, minecraftName=?");
+$stmt->bind_param("sssssss", $ip, $date, $activeDate, $username, $date, $activeDate, $username);
 $stmt->execute();
 $stmt->close();
 unset($stmt);
