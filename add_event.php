6160e1c166e3d4f6a2edcefebec0e2d2db3d2b7b
inventivetalent <mail@inventivetalent.org>
2019-08-13 17:24:27 +0200

    increase

--- add_event.php
+++ add_event.php
@@ -22,7 +22,7 @@ $username = isset($_POST["username"]) ? $_POST["username"] : "";
 //    die("missing captcha");
 //}
 
-$confirmationCheckFactor = 80;
+$confirmationCheckFactor = 90;
 
 //die("*sigh* I said don't abuse plz :(");
 
