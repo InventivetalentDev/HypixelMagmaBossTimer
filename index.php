7ee03777c0b266fb2c36183e81757541688f3f10
inventivetalent <mail@inventivetalent.org>
2019-08-20 20:24:48 +0200

    redirect

--- index.php
+++ index.php
@@ -209,6 +209,12 @@
             </div>
         </div>
 
+        <script>
+            if (location.host !== "hypixel.inventivetalent.org") {
+                window.location = "https://hypixel.inventivetalent.org/skyblock-magma-timer";
+            }
+        </script>
+
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.4.1/jquery.min.js" integrity="sha256-CSXorXvZcTkaix6Yvo6HppcZGetbYMGWSFlBw8HfCJo=" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/js/materialize.min.js"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.24.0/moment.min.js" integrity="sha256-4iQZ6BVL4qNKlQ27TExEhBN1HFPvAvAMbFavKKosSWQ=" crossorigin="anonymous"></script>
